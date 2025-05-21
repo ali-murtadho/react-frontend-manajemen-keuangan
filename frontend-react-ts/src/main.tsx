752d641b63246d9c589033397b9a9f4fa8c321b2
Ali Murtadho <ali.murtadho@lawangsewu.com>
2025-05-21 17:44:40 +0700

    feat: base react ts fe

---
+++ frontend-react-ts/src/main.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.tsx'
+
+// //import BrowserRouter dari react router
+// import { BrowserRouter } from 'react-router';
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
