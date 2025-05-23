0b881e2b3c390183fd83d91c3bf68010d03f646b
Ali Murtadho <ali.murtadho@lawangsewu.com>
2025-05-23 13:48:00 +0700

    feat: auth hooks

--- frontend-react-ts/src/main.tsx
+++ frontend-react-ts/src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
 
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
@@ -23,3 +23,29 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 //     </BrowserRouter>
 //   </StrictMode>,
 // )
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+//import BrowserRouter dari react router
+import { BrowserRouter } from 'react-router';
+
+//import QueryClient dan QueryClientProvider dari react-query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider } from './context/AuthContext.tsx';
+
+//init QueryClient
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>,
+)
